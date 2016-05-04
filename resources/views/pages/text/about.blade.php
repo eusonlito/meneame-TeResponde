410874f123f726c459315865cc9df90b10b4dc08
Lito <lito@eordes.com>
2016-05-05 00:18:35 +0200

    Updated about info

--- resources/views/pages/text/about.blade.php
+++ resources/views/pages/text/about.blade.php
@@ -22,9 +22,9 @@
             <ul>
                 <li>El código fuente lo tienes disponible en <a href="https://github.com/eusonlito/meneame-responde" target="_blank">GitHub</a></li>
                 <li>Las imágenes utilizadas en las cabeceras son obras Creative Commons obtenidas de <a href="http://www.uhdwallpapers.org/p/creative-commons-images.html" target="_blank">http://www.uhdwallpapers.org/p/creative-commons-images.html</a></li>
-                <li>El tema utilizado es el distriubido bajo licencia MIT por <a href="https://github.com/BlackrockDigital/startbootstrap-clean-blog" target="_blank">https://github.com/BlackrockDigital/startbootstrap-clean-blog</a></li>
+                <li>El tema utilizado es el distribuido bajo licencia MIT por <a href="https://github.com/BlackrockDigital/startbootstrap-clean-blog" target="_blank">https://github.com/BlackrockDigital/startbootstrap-clean-blog</a></li>
                 <li>El framework de desarrollo es <a href="https://lumen.laravel.com/" target="_blank">Lumen</a></li>
-                <li>El servidor usa nginx + PHP7-FPM + MySQL 5.7</li>
+                <li>El servidor usa Ubuntu 16.04 + nginx + PHP7-FPM + MySQL 5.7</li>
                 <li>Yo soy <a href="https://about.me/lito" target="_blank">Lito</a></li>
                 <li>Este proyecto me ha llevado unas <a href="https://github.com/eusonlito/meneame-responde" target="_blank">10 horas</a></li>
             </ul>
